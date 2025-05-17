d62e198155383b432cf9211b65d5fb39fd160291
Laura Romero <laura.romero@abptecnologica.com>
2025-05-17 17:57:23 +0200

    Separamos el componente en un fragmento que usar dentro de otro componente

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,20 +1,30 @@
+import { Fragment } from "react/jsx-runtime";
+
 function Card() {
-  const width = {
-    width: "350px",
-  };
   return (
-    <div className="card" style={{ width: "350px" }}>
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card’s content.
-        </p>
-        <a href="#" className="btn btn-primary">
-          Go somewhere
-        </a>
-      </div>
+    <div
+      className="card"
+      style={{
+        width: "350px",
+      }}
+    >
+      <div className="card-body"><CardBody /></div>
     </div>
+  );
+}
+
+function CardBody() {
+  return (
+    <Fragment>
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">
+        Some quick example text to build on the card title and make up the bulk
+        of the card’s content.
+      </p>
+      <a href="#" className="btn btn-primary">
+        Go somewhere
+      </a>
+    </Fragment>
   );
 }
 
