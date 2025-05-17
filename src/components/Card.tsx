53c4c5b158e240478427a608d635c09abca65ec7
Laura Romero <laura.romero@abptecnologica.com>
2025-05-17 18:32:57 +0200

    Pasando Múltiples props

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,9 @@
 interface CardProps {
-    body: string;
-    }
+  body: string;
+}
 
 function Card(props: CardProps) {
-    const { body } = props;
+  const { body } = props;
   return (
     <div
       className="card"
@@ -16,18 +16,18 @@ function Card(props: CardProps) {
   );
 }
 
-function CardBody() {
+interface CardBodyProps {
+  title: string;
+  text?: string;
+}
+
+export function CardBody(props: CardBodyProps) {
+  const { title, text } = props;
   return (
-    <Fragment>
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">
-        Some quick example text to build on the card title and make up the bulk
-        of the card’s content.
-      </p>
-      <a href="#" className="btn btn-primary">
-        Go somewhere
-      </a>
-    </Fragment>
+    <>
+      <h5 className="card-title">{title}</h5>
+      <p className="card-text">{text}</p>
+    </>
   );
 }
 
